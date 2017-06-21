0de4ecece60d127e12951999998b13eece108fcd
Osagie Omonzokpia <osagieomon@Osagies-MacBook-Pro.local>
2017-06-21 21:41:56 +0100

    Media player

--- app/src/main/java/com/dreammesh/app/bakingapp/ui/fragment/RecipeStepFragment.java
+++ app/src/main/java/com/dreammesh/app/bakingapp/ui/fragment/RecipeStepFragment.java
@@ -1,17 +1,15 @@
 package com.dreammesh.app.bakingapp.ui.fragment;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
-import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.dreammesh.app.bakingapp.R;
 import com.dreammesh.app.bakingapp.data.model.Step;
@@ -20,7 +18,9 @@ import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
@@ -31,6 +31,7 @@ import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
@@ -40,15 +41,30 @@ import static com.dreammesh.app.bakingapp.util.CommonUtil.STEP_FRAGMENT_KEY;
 
 /**
  * A placeholder fragment containing a simple view.
+ * Help from: https://gist.github.com/olushi/c26bb8a5f27f94d73b3a4888a509927c
  */
-public class RecipeStepFragment extends Fragment implements ExoPlayer.EventListener{
+public class RecipeStepFragment extends Fragment implements
+        ExoPlayer.EventListener {
+
+    // constant fields for saving and restoring bundle
+    public static final String AUTOPLAY = "autoplay";
+    public static final String CURRENT_WINDOW_INDEX = "current_window_index";
+    public static final String PLAYBACK_POSITION = "playback_position";
+    public static final String VIDEO_URL = "video_url";
 
     @BindView(R.id.recipe_step_video)
     SimpleExoPlayerView exoPlayerView;
 
-    SimpleExoPlayer exoPlayer;
+    private SimpleExoPlayer mPlayer;
     private MediaSessionCompat mediaSession;
-    private PlaybackStateCompat.Builder stateBuilder;
+
+    // autoplay = false
+    private boolean autoPlay = false;
+
+    // used to remember the playback position
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
+    private String videoUrl;
 
     public RecipeStepFragment() {}
 
@@ -74,92 +90,45 @@ public class RecipeStepFragment extends Fragment implements ExoPlayer.EventListe
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Step step = getArguments().getParcelable(STEP_FRAGMENT_KEY);
+        // if we have saved player state, restore it
+        if (savedInstanceState != null) {
+            videoUrl = savedInstanceState.getString(VIDEO_URL);
+            playbackPosition = savedInstanceState.getLong(PLAYBACK_POSITION, 0);
+            currentWindow = savedInstanceState.getInt(CURRENT_WINDOW_INDEX, 0);
+            autoPlay = savedInstanceState.getBoolean(AUTOPLAY, false);
 
-        assert step != null;
-        String video = step.getVideoURL();
+            initViews(videoUrl);
+        } else {
+            Step step = getArguments().getParcelable(STEP_FRAGMENT_KEY);
+            String video = null;
+            if (step != null) {
+                video = step.getVideoURL();
+            }
+            initViews(video);
+        }
+    }
 
-        if (video != null && !video.isEmpty()) {
-            // Init and show video view
+    private void initViews(String videoUrl) {
+        if(videoUrl != null && !videoUrl.isEmpty()) {
             setViewVisibility(exoPlayerView, true);
+
             initializeMediaSession();
-            initializePlayer(Uri.parse(video));
+            initializePlayer(Uri.parse(videoUrl));
         } else {
             setViewVisibility(exoPlayerView, false);
         }
     }
 
-    private void setViewVisibility(View view, boolean show) {
-        if (show) {
-            view.setVisibility(View.VISIBLE);
-        } else {
-            view.setVisibility(View.GONE);
-        }
+    private void initializePlayer(Uri parse) {
+
     }
 
     private void initializeMediaSession() {
 
-        mediaSession = new MediaSessionCompat(getContext(), "RecipeStepSinglePageFragment");
-
-        mediaSession.setFlags(
-                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
-                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
-
-        mediaSession.setMediaButtonReceiver(null);
-        stateBuilder = new PlaybackStateCompat.Builder()
-                .setActions(
-                        PlaybackStateCompat.ACTION_PLAY |
-                                PlaybackStateCompat.ACTION_PAUSE |
-                                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
-                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
-
-        mediaSession.setPlaybackState(stateBuilder.build());
-        mediaSession.setCallback(new MediaSessionCompat.Callback() {
-            @Override
-            public void onPlay() {
-                exoPlayer.setPlayWhenReady(true);
-            }
-
-            @Override
-            public void onPause() {
-                exoPlayer.setPlayWhenReady(false);
-            }
-
-            @Override
-            public void onSkipToPrevious() {
-                exoPlayer.seekTo(0);
-            }
-        });
-        mediaSession.setActive(true);
     }
 
-    private void initializePlayer(Uri mediaUri) {
-        if (exoPlayer == null) {
+    private void setViewVisibility(View view, boolean b) {
 
-            TrackSelector trackSelector = new DefaultTrackSelector();
-            LoadControl loadControl = new DefaultLoadControl();
-            exoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
-            exoPlayerView.setPlayer(exoPlayer);
-            exoPlayer.addListener(this);
-
-            String userAgent = Util.getUserAgent(getContext(), "StepVideo");
-            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
-                    getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
-            exoPlayer.prepare(mediaSource);
-            exoPlayer.setPlayWhenReady(true);
-        }
-    }
-
-    private void releasePlayer() {
-        if (exoPlayer != null) {
-            exoPlayer.stop();
-            exoPlayer.release();
-            exoPlayer = null;
-        }
-
-        if (mediaSession != null) {
-            mediaSession.setActive(false);
-        }
     }
 
     @Override
@@ -192,12 +161,8 @@ public class RecipeStepFragment extends Fragment implements ExoPlayer.EventListe
 
     }
 
-    /**
-     * Release the player when the activity is destroyed.
-     */
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        releasePlayer();
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
     }
 }
